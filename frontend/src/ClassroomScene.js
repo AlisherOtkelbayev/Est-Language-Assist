c1793d9240ef333b8329d19f94895b91ef5686ce
Alisher Otkelbayev <alisher.otkelbayev@gmail.com>
2025-03-14 01:56:36 +0200

    3d classroom added

---
+++ frontend/src/ClassroomScene.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+function Classroom() {
+  const { scene } = useGLTF("/models/Classroom.glb");
+    //<div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [2, 2, 5], fov: 30 }}>
+        <ambientLight intensity={2.0} />
+        <directionalLight position={[2, 5, 2]} intensity={1} />
+        <primitive object={scene} scale={3} position={[15, -13, 6]} />
+        <OrbitControls enableZoom={true} enablePan={false} />
+      </Canvas>
+   //</div>
+}
+export default Classroom;
