2da53fafe61fc8a8bcebaef2ccccc06793c12311
Alisher Otkelbayev <alisher.otkelbayev@gmail.com>
2025-03-14 02:37:49 +0200

    fixed 3d classroom

--- frontend/src/ClassroomScene.js
+++ frontend/src/ClassroomScene.js
@@ -5,12 +5,16 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 function Classroom() {
   const { scene } = useGLTF("/models/Classroom.glb");
     //<div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [2, 2, 5], fov: 30 }}>
+    return (
+        <Canvas camera={{position: [2, 2, 5], fov: 35}}
+        style={{ width: "100vw", height: "100vh" }}
+        >
         <ambientLight intensity={2.0} />
         <directionalLight position={[2, 5, 2]} intensity={1} />
-        <primitive object={scene} scale={3} position={[15, -13, 6]} />
+        <primitive object={scene} scale={3} position={[17, -11, 6]} />
         <OrbitControls enableZoom={true} enablePan={false} />
       </Canvas>
    //</div>
+    );
 }
 export default Classroom;
